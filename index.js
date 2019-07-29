526c3bea2a3197cf19bdd5614e5730bbc947fa65
Mariam Anuashvili <m.anuashvili@gmail.com>
2019-07-29 18:04:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,18 +15,6 @@ function nowServing(katzDeli) {
   return (`Currently serving ${katzDeli.shift()}.`);
 }
 
-function currentLine(katzDeli) {
-  let i = 0;
-  while (i < katzDeli.length) {
-    line.push(` `+[i+1]+`. `  + katzDeli[i])
-    i++;
-  }
-  if (katzDeli.length === 0) {
-    return "The line is currently empty.";
-  } else
-  return(`The line is currently:` + line);
-}
-
 function currentLine(line) {
   if (line.length === 0) {
     return `The line is currently empty.`;
